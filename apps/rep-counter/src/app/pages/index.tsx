15dbd00d414be0ea1e99f1c7487044782c69568e
Flyyn <_@rst.vn>
2023-01-22 19:44:59 -0500

    Add (minimal) homepage content

--- apps/rep-counter/src/app/pages/index.tsx
+++ apps/rep-counter/src/app/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { DashboardLayout } from "../layout/DashboardLayout";
 
 export function LandingPage() {
   return (
     <DashboardLayout>
       <Container maxW="container.md">
+        <Link to="/start/">Use quick-start</Link>
       </Container>
     </DashboardLayout>
   );
